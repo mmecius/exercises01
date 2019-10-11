fa5451c699bc294caf658a44462846b16da05ad7
mmecius <tmecius@gmail.com>
2019-10-11 18:01:55 +0300

    first commit

---
+++ Exercises01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exercises01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
